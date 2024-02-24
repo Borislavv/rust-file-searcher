d50895696ec88b67defd6dd118aaae5cc0b46fa3
Boris <Glazunov2142@icloud.com>
2024-02-24 12:49:43 +0300

    chore: [WIP] improved Arg struct and parsing of it.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,36 @@
+use std::error::Error;
+use std::string::ToString;
 use clap::Parser;
+
+const ROOT_DIR: String = "/".to_string();
 
 #[derive(Parser, Debug)]
 struct Args {
+    /// Directory in which the search will be performed
     #[arg(short, long)]
     dir: String,
 
+    /// Target file name for search
     #[arg(short, long)]
     file: String,
 
+    /// Acceptable found files extensions
     #[arg(short, long, value_delimiter = ',')]
     extensions: Vec<String>
 }
 
 fn main() {
-    let args = Args::parse();
+    let args = parse_args();
 
     println!("{:?}", args);
+}
+
+fn parse_args() -> Result<Args, dyn Error> {
+    let mut args = Args::parse();
+
+    if args.dir.is_empty() {
+        args.dir = ROOT_DIR
+    }
+
+    return Ok(args);
 }
