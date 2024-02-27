e6bc0e8c99be51c8b1eae6573a08235f8cf15181
Boris <Glazunov2142@icloud.com>
2024-02-27 20:50:24 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::rc::Rc;
 use std::sync::Arc;
-use std::sync::mpsc::Sender;
+use std::sync::mpsc::{Receiver, Sender};
 use clap::Parser;
+use tokio::join;
 
 const ROOT_DIR: &str = "/";
 
@@ -14,10 +14,6 @@ struct Config {
     /// Target file name for search
     #[arg(short, long)]
     file: String,
-
-    /// Acceptable found files extensions
-    #[arg(short, long, value_delimiter = ',')]
-    extensions: Vec<String>
 }
 
 #[tokio::main]
@@ -27,22 +23,34 @@ async fn main() {
         Err(err) => panic!("{}", err)
     });
 
-    let (sender, mut receiver) = std::sync::mpsc::channel::<&str>();
+    let file = cfg.file.clone();
+    let dir  = cfg.dir.clone();
 
-    let mut find_handle = tokio::spawn(find(sender, cfg).await?);
+    let (file_tx, file_rx) = std::sync::mpsc::channel::<String>();
+    let (err_tx, err_rx) = std::sync::mpsc::channel::<String>();
 
-    let mut i = 1;
-    for path in receiver.iter() {
-        println!("#{}: {}\n", i, path);
-        i += 1;
-    }
+    let files_handle  = tokio::spawn(print_files(file_rx));
+    let errors_handle = tokio::spawn(print_errors(err_rx));
+    let find_handle   = tokio::spawn(find(file_tx, err_tx, file, dir));
 
-    find_handle.await?;
-
-    println!("{:?}", cfg);
+    join!(find_handle, files_handle, errors_handle);
 }
 
-fn parse_args() -> Result<Config, &'static str> {
+async fn print_files(files_rx: Receiver<String>) {
+    let mut i = 1;
+    for f in files_rx.iter() {
+        println!("#{}: {}", i, f);
+        i += 1;
+    }
+}
+
+async fn print_errors(errors_rx: Receiver<String>) {
+    for e in errors_rx.iter() {
+        println!("error: {}", e);
+    }
+}
+
+fn parse_args() -> Result<Config, String> {
     let mut cfg = Config::parse();
 
     if cfg.dir.is_empty() {
@@ -50,14 +58,52 @@ fn parse_args() -> Result<Config, &'static str> {
     }
 
     if cfg.file.is_empty() {
-        return Err("file cannot be empty or omitted");
+        return Err("file cannot be empty or omitted".to_string());
     }
 
     return Ok(cfg);
 }
 
-async fn find(ch: Sender<&str>, cfg: &Arc<Config>)  {
-    for entry in std::fs::read_dir(cfg.dir.clone()).unwrap() {
-        let dir = entry.unwrap().;
-    }
+async fn find(file_tx: Sender<String>, err_tx: Sender<String>, file: String, dir: String)  {
+    let dir_entries = match std::fs::read_dir(dir.as_str()) {
+        Ok(dir_entries) => dir_entries,
+        Err(err) => {
+            err_tx.send(err.to_string());
+            return;
+        }
+    };
+
+    dir_entries.for_each(|entry| {
+        let dir_entry = match entry {
+            Ok(dir_entry) => dir_entry,
+            Err(err) => {
+                err_tx.send(err.to_string());
+                return;
+            }
+        };
+
+        if dir_entry.file_name().to_str().unwrap() == &*file {
+            file_tx.send(format!(
+                "{}/{}", dir, dir_entry.file_name().to_os_string().to_str().unwrap()
+            ));
+        }
+
+        if dir_entry.path().is_dir() {
+            let mut into_dir: String;
+            if dir_entry.path().to_str().unwrap_or("/") == "/" {
+                into_dir = "/".to_string();
+            } else {
+                into_dir = format!(
+                    "{}/{}", dir, dir_entry.path().to_str().unwrap().to_string()
+                )
+            }
+
+            let file_tx_cloned = file_tx.clone();
+            let err_tx_cloned = err_tx.clone();
+            let file_cloned = file.clone();
+            let dir_cloned = dir.clone();
+
+            tokio::spawn(find(file_tx_cloned, err_tx_cloned, file_cloned, dir_cloned));
+        }
+    });
 }
